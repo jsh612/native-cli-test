8c748379fa1a0496a238edb7d1f48d5fb299c5d3
seungho Jeon <urijsh612@gmail.com>
2020-04-14 18:03:29 +0900

    #3. react-native-vector-icons link 방법

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {Button} from 'react-native-paper';
+import styled from 'styled-components';
+import {View} from 'react-native';
 
 declare const global: {HermesInternal: null | {}};
 
+const SView = styled(View)`
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
 const App = () => {
-  return <Button icon="camera">안녕</Button>;
+  return (
+    <SView>
+      <Button icon="camera">안녕</Button>
+    </SView>
+  );
 };
 
 export default App;
