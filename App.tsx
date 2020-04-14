9f42f9aad8719f6fdf0e16805f072f0ea926e3ed
seungho Jeon <urijsh612@gmail.com>
2020-04-14 17:56:53 +0900

    init

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from 'react-native-paper';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return <Button icon="camera">안녕</Button>;
+};
+
+export default App;
